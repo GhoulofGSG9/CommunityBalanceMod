ec671cd9232480fb511790004fb41bff0137a4d8
Katzenfleisch <Katzenfleisch@users.noreply.github.com>
2026-06-03 12:40:53 +0200

    Code reorder to shorten critical path to DoDamage()

--- src/lua/Weapons/BulletsMixin.lua
+++ src/lua/Weapons/BulletsMixin.lua
@@ -24,24 +24,10 @@ end
 -- check for umbra and play local hit effects (bullets only)
 function BulletsMixin:ApplyBulletGameplayEffects(player, target, endPoint, direction, damage, surface, showTracer, weaponAccuracyGroupOverride)
 
-    -- Handle Stats
-    if Server then
 
-        local parent = self and self.GetParent and self:GetParent()
-        if parent and self.GetTechId then
-
-            -- Drifters, buildings and teammates don't count towards accuracy as hits or misses
-            if (target and target:isa("Player") and GetAreEnemies(parent, target)) or target == nil then
-
-                local steamId = parent:GetSteamId()
-                if steamId then
-                    StatsUI_AddAccuracyStat(steamId, self:GetTechId(), target ~= nil, target and target:isa("Onos"), parent:GetTeamNumber())
-                end
-            end
-            GetBotAccuracyTracker():AddAccuracyStat(parent:GetClient(), target ~= nil, weaponAccuracyGroupOverride or kBotAccWeaponGroup.Bullets)
-        end
-    end
-
+    local hasTarget = target ~= nil
+    local isTargetEnemyPlayer = target and target:isa("Player") and GetAreEnemies(parent, target)
+    local isTargetOnos = target and target:isa("Onos")
     local blockedByUmbra = GetBlockedByUmbra(target)
     
     if blockedByUmbra then
@@ -50,5 +36,23 @@ function BulletsMixin:ApplyBulletGameplayEffects(player, target, endPoint, direc
 
     -- deals damage or plays surface hit effects
     self:DoDamage(damage, target, endPoint, direction, surface, false, showTracer)
+
+    -- Handle Stats
+    if Server then
+
+        local parent = self and self.GetParent and self:GetParent()
+        if parent and self.GetTechId then
+
+            -- Drifters, buildings and teammates don't count towards accuracy as hits or misses
+            if isTargetEnemyPlayer or target == nil then
+
+                local steamId = parent:GetSteamId()
+                if steamId then
+                    StatsUI_AddAccuracyStat(steamId, self:GetTechId(), hasTarget, isTargetOnos, parent:GetTeamNumber())
+                end
+            end
+            GetBotAccuracyTracker():AddAccuracyStat(parent:GetClient(), hasTarget, weaponAccuracyGroupOverride or kBotAccWeaponGroup.Bullets)
+        end
+    end
     
 end
