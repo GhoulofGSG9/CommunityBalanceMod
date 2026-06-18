1b13ea6ace8545b635b99fbaa1eb42264c8756de
Katzenfleisch <Katzenfleisch@users.noreply.github.com>
2026-06-18 15:33:37 +0200

    Optimized bonewall
    - Prevents SetOrigin and math functions to  be called unless necessary. SetOrigin has many implication especially in regard to collisions and hitboxes where it forces a recheck. Do not do that if the entity is not moving

--- src/lua/CommAbilities/Alien/BoneWall.lua
+++ src/lua/CommAbilities/Alien/BoneWall.lua
@@ -133,23 +133,21 @@ end
 
 function BoneWall:OnUpdate(deltaTime)
 
+    PROFILE("BoneWall:OnUpdate")
+
     CommanderAbility.OnUpdate(self, deltaTime)
     
     local lifeTime = math.max(0, Shared.GetTime() - self:GetTimeCreated())
-    local remainingTime = self:GetLifeSpan() - lifeTime
-    
-    if remainingTime < self:GetLifeSpan() then
-        
-        local moveFraction = 0
+    local lifeSpan = self:GetLifeSpan()
+    local remainingTime = lifeSpan - lifeTime
+    local moveFraction = 0
+    local piFraction = 0
 
-        if remainingTime <= 1 then
-            moveFraction = 1 - Clamp(remainingTime / kMoveDuration, 0, 1)
-        end
-        
-        local piFraction = moveFraction * (math.pi / 2)
+    if remainingTime <= 1 then
+        moveFraction = 1 - Clamp(remainingTime / kMoveDuration, 0, 1)
+        piFraction = moveFraction * (math.pi / 2)
 
         self:SetOrigin(self.spawnPoint - Vector(0, math.sin(piFraction) * kMoveOffset, 0))
-    
     end
 
 end
