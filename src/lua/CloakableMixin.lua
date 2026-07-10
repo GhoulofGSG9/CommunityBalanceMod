c2161884f581046ae767fb123381cc38fa084e5c
Katzenfleisch <Katzenfleisch@users.noreply.github.com>
2026-07-10 11:06:18 +0200

    Reduce delay to 1s and adds netvar check

--- src/lua/CloakableMixin.lua
+++ src/lua/CloakableMixin.lua
@@ -292,9 +292,10 @@ local function UpdateCloakState(self, deltaTime)
     PROFILE("CloakableMixin:OnUpdate")
 
     -- Do not run the expensive code next if we never cloaked, only check cloak rate for those who already got camoed
-    if (self.hasCloakedOnce == false and self.timeLastFirstCloakUpdateCheck + 1.5 > Shared.GetTime()) then
+    if (self.cloakFraction == 0 and self.hasCloakedOnce == false and self.timeLastFirstCloakUpdateCheck + 1 > Shared.GetTime()) then
         return
     end
+
     self.timeLastFirstCloakUpdateCheck = Shared.GetTime()
 
     -- Account for trigger cloak, uncloak, camouflage speed
