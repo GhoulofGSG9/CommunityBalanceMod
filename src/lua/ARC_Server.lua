f5620657bee35a5d205721c2a13995e6a3a3e07f
Katzenfleisch <Katzenfleisch@users.noreply.github.com>
2026-07-24 14:45:16 +0200

    ARC_Server.lua - removes the dirty blip marking

--- src/lua/ARC_Server.lua
+++ src/lua/ARC_Server.lua
@@ -13,10 +13,6 @@ local kMoveParam = "move_speed"
 
 function ARC:OnEntityChange(oldId)
 
-    if HasMixin(self, "MapBlip") then 
-        self:MarkBlipDirty()
-    end
-
     if self.targetedEntity == oldId then
         self.targetedEntity = Entity.invalidId
     end   
