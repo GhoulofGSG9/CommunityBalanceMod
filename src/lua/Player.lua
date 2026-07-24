48ef8b278a38b3ea8dba6ed8779c7ce66a61b202
Katzenfleisch <Katzenfleisch@users.noreply.github.com>
2026-07-24 20:40:41 +0200

    Update Player.lua - condition reordering

--- src/lua/Player.lua
+++ src/lua/Player.lua
@@ -1892,7 +1892,7 @@ function Player:GetIsMoveable()
 end
 
 function Player:GetIsIdle()
-    return self:GetVelocityLength() < 0.1 and not self.moveButtonPressed
+    return not self.moveButtonPressed and self:GetVelocityLength() < 0.1
 end
 
 function Player:GetPlayIdleSound()
@@ -2515,12 +2515,10 @@ kStepTagNames["step_crouch"] = true
 function Player:OnTag(tagName)
 
     PROFILE("Player:OnTag")
-    local crouching = HasMixin(self, "CrouchMove") and self:GetCrouching()
-
     -- Log("%s: tag %s (%s)", self, tagName, kStepTagNames[tagName])
 
     -- Filter out crouch steps from playing at inappropriate times.
-    if tagName == "step_crouch" and not crouching then
+    if tagName == "step_crouch" and not (HasMixin(self, "CrouchMove") and self:GetCrouching()) then
         return
     end
 
