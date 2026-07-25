7c6e276be9c92561f472dec359fbc1d216088a63
NexZone30 <108672773+NexZone30@users.noreply.github.com>
2026-07-24 22:49:40 -0500

    Onos cloak exception

--- src/lua/CloakableMixin.lua
+++ src/lua/CloakableMixin.lua
@@ -574,7 +574,7 @@ function CloakableMixin:GetIsInInk()
 end
 
 function CloakableMixin:GetInvisibleRange()
-    return (CloakableMixin.kSpecialMaxCloakClass[self:GetClassName()] and 1.00) or CloakableMixin.kInvisibleFarRange[self.cloakRate] or CloakableMixin.kInvisibleFarRange[1]
+    return (CloakableMixin.kSpecialMaxCloakClass[self:GetClassName()] and not self:isa("Onos") and 1.0) or CloakableMixin.kInvisibleFarRange[self.cloakRate] or CloakableMixin.kInvisibleFarRange[1]
 end
 
 function CloakableMixin:OverrideCheckVisibilty(viewer)
