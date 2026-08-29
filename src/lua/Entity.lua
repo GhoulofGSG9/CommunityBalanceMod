d5158747cf7f4567516b8a3eff0cba16f0855cda
Katzenfleisch <Katzenfleisch@users.noreply.github.com>
2026-08-29 11:00:51 +0200

    Adding RadiusDamageMultiPoint() function
    - This will allow for radius damaging entities to provide a multitude of points to check damages from, such as mines.

--- src/lua/Entity.lua
+++ src/lua/Entity.lua
@@ -501,7 +501,7 @@ if jit.os == "Linux" then
 end
 
 -- Fades damage linearly from center point to radius (0 at far end of radius)
-function RadiusDamage(entities, centerOrigin, radius, fullDamage, doer, ignoreLOS, fallOffFunc, useXZDistance)
+function RadiusDamageMultiPoint(entities, centerOrigins, radius, fullDamage, doer, ignoreLOS, fallOffFunc, useXZDistance)
 
     assert(HasMixin(doer, "Damage"))
 
@@ -509,45 +509,55 @@ function RadiusDamage(entities, centerOrigin, radius, fullDamage, doer, ignoreLO
 
     -- Do damage to every target in range
     for _, target in ipairs(entities) do
-    
-        -- Find most representative point to hit
+
         local targetOrigin = GetTargetOrigin(target)
-
-        local distanceVector = targetOrigin - centerOrigin
-
-        -- Trace line to each target to make sure it's not blocked by a wall
-        local wallBetween = false
-        local distanceFromTarget
-        if useXZDistance then
-            distanceFromTarget = distanceVector:GetLengthSquaredXZ()
-        else
-            distanceFromTarget = distanceVector:GetLengthSquared()
-        end
-
-        if not ignoreLOS then
-            wallBetween = GetWallBetween(centerOrigin, targetOrigin, target)
-        end
+        for i, centerOrigin in ipairs(centerOrigins) do
         
-        if (ignoreLOS or not wallBetween) and (distanceFromTarget <= radiusSquared) then
-        
-            -- Damage falloff
-            local distanceFraction = distanceFromTarget / radiusSquared
-            if fallOffFunc then
-                distanceFraction = fallOffFunc(distanceFraction)
+            -- Find most representative point to hit
+            local distanceVector = targetOrigin - centerOrigin
+
+            -- Trace line to each target to make sure it's not blocked by a wall
+            local wallBetween = false
+            local distanceFromTarget
+            if useXZDistance then
+                distanceFromTarget = distanceVector:GetLengthSquaredXZ()
+            else
+                distanceFromTarget = distanceVector:GetLengthSquared()
             end
-            distanceFraction = Clamp(distanceFraction, 0, 1)
 
-            local damage = fullDamage * (1 - distanceFraction)
-
-            local damageDirection = distanceVector
-            damageDirection:Normalize()
+            if not ignoreLOS then
+                wallBetween = GetWallBetween(centerOrigin, targetOrigin, target)
+            end
             
-            -- we can't hit world geometry, so don't pass any surface params and let DamageMixin decide
-            doer:DoDamage(damage, target, target:GetOrigin(), damageDirection, "none")
+            if (ignoreLOS or not wallBetween) and (distanceFromTarget <= radiusSquared) then
+            
+                -- Damage falloff
+                local distanceFraction = distanceFromTarget / radiusSquared
+                if fallOffFunc then
+                    distanceFraction = fallOffFunc(distanceFraction)
+                end
+                distanceFraction = Clamp(distanceFraction, 0, 1)
 
+                local damage = fullDamage * (1 - distanceFraction)
+
+                local damageDirection = distanceVector
+                damageDirection:Normalize()
+                
+                -- we can't hit world geometry, so don't pass any surface params and let DamageMixin decide
+                doer:DoDamage(damage, target, target:GetOrigin(), damageDirection, "none")
+                break -- One of the point provided dealt damages, don't overdamage
+
+            end
         end
         
     end
+    
+end
+
+-- Fades damage linearly from center point to radius (0 at far end of radius)
+function RadiusDamage(entities, centerOrigin, radius, fullDamage, doer, ignoreLOS, fallOffFunc, useXZDistance)
+
+    RadiusDamageMultiPoint(entities, {centerOrigin}, radius, fullDamage, doer, ignoreLOS, fallOffFunc, useXZDistance)
     
 end
 
