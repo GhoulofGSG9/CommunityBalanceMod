7fb4ed87fc30ab7d805bb31db82367d80696c9bc
Katzenfleisch <Katzenfleisch@users.noreply.github.com>
2026-09-05 01:09:56 +0200

    Update Player.lua - passing the trace use-points coords

--- src/lua/Player.lua
+++ src/lua/Player.lua
@@ -941,7 +941,7 @@ function Player:PerformUseTrace()
 
 end
 
-function Player:UseTarget(entity, timePassed)
+function Player:UseTarget(entity, timePassed, usePoint)
 
     assert(entity)
 
@@ -949,7 +949,7 @@ function Player:UseTarget(entity, timePassed)
     if entity.OnUse then
 
         useSuccessTable.useSuccess = true
-        entity:OnUse(self, timePassed, useSuccessTable)
+        entity:OnUse(self, timePassed, useSuccessTable, usePoint)
 
     end
 
@@ -999,7 +999,7 @@ local function AttemptToUse(self, timePassed)
         end
         
         -- Use it.
-        if self:UseTarget(entity, elapsedTime) then
+        if self:UseTarget(entity, elapsedTime, usablePoint) then
 
             if kDebugBuildTimes and HasMixin(entity, "Construct") and not entity:GetIsBuilt() then
 
