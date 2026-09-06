bacc4df7028f268a62265da8e826618261115f18
Katzenfleisch <Katzenfleisch@users.noreply.github.com>
2026-09-06 19:38:14 +0200

    Update FogOfWarEntity.lua - adding fade-out for pve

--- src/lua/FogOfWarEntity.lua
+++ src/lua/FogOfWarEntity.lua
@@ -7,7 +7,8 @@ class 'FogOfWarEntity' (ScriptActor)
 
 FogOfWarEntity.kMapName = "fogOfwarentity"
 
-kFogPlayerBlipLifetime = 30
+kFogBlipLifetime = 30
+kFogPlayerBlipLifetime = 3
 kFogPlayerBlipFadeTime = 2
 
 local networkVars =
@@ -27,7 +28,7 @@ local kFogEntRelevancyDist = kPlayerLOSDistance
 local kFogEndUpdateInterval = 0.8
 
 function FogOfWarEntity:OnCreate()
-	ScriptActor.OnCreate(self)
+    ScriptActor.OnCreate(self)
 
     InitMixin(self, TeamMixin)
 
@@ -48,7 +49,7 @@ function FogOfWarEntity:OnUpdate()
         return
     end
 
-    if self.isPlayerBlip and self.fogExpireTime and Shared.GetTime() >= self.fogExpireTime then
+    if self.fogExpireTime and Shared.GetTime() >= self.fogExpireTime then
         self:SetFogEntMapBlipInfo(false)
         return
     end
@@ -99,11 +100,11 @@ function FogOfWarEntity:SetFogEntMapBlipInfo(visible, blipType, blipTeam, isActi
     self.visible = visible
 
     if blipType ~= nil then
-	   self.blipType = blipType
+       self.blipType = blipType
     end
 
     if blipTeam ~= nil then
-	   self.blipTeam = blipTeam
+       self.blipTeam = blipTeam
        self:SetTeamNumber(blipTeam)
     end
 
@@ -115,8 +116,9 @@ function FogOfWarEntity:SetFogEntMapBlipInfo(visible, blipType, blipTeam, isActi
        self.isPlayerBlip = isPlayerBlip
     end
 
-    if visible and self.isPlayerBlip and not wasVisible then
-        self.fogExpireTime = Shared.GetTime() + kFogPlayerBlipLifetime + kFogPlayerBlipFadeTime
+    if visible and not wasVisible then
+        local lifetime = self.isPlayerBlip and kFogPlayerBlipLifetime or kFogBlipLifetime
+        self.fogExpireTime = Shared.GetTime() + lifetime + kFogPlayerBlipFadeTime
     elseif not visible then
         self.fogExpireTime = nil
     end
