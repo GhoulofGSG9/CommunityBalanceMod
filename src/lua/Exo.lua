4a97ec1b53c612fc150db3ede5218ae909fe50ac
GhoulofGSG9 <ghoulofgsg9@gmail.com>
2026-09-09 12:40:01 +0200

    docs(exo): fix the vertical thrust fuel comment

--- src/lua/Exo.lua
+++ src/lua/Exo.lua
@@ -1770,7 +1770,7 @@ function Exo:GetFuelUsageRate()
     --local usageScalar = self:GetHasMinigun() and kMinigunFuelUsageScalar or kRailgunFuelUsageScalar
     if self.thrustersActive then
 		if self.thrusterMode == kExoThrusterMode.Vertical then
-			return kExoThrusterFuelUsageRate * 0.33 -- This is triple fuel consumption
+			return kExoThrusterFuelUsageRate * 0.33 -- Vertical thrust burns a third of the rate
 		else 
 			return kExoThrusterFuelUsageRate --* usageScalar
 		end
